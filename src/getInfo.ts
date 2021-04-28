7f7e1ada12066d4e3d1f0ad5ffbd0d593245e23f
Norbert Bodziony <norbertbodziony@gmail.com>
2021-04-28 14:22:30 +0200

    fix vesting

--- src/getInfo.ts
+++ src/getInfo.ts
@@ -14,10 +14,9 @@ import { Schedule } from './state'
 const connection = new Connection('https://api.mainnet-beta.solana.com')
 const TOKEN_ADDRESS = new PublicKey('xuNGfwG8r3CEubQcssj3A7zoZwb4NaiamhHsJEMt7kf')
 const SOURCE_TOKENS_ADDRESS = new PublicKey('35QuD9QWJDSLKJEmYrfuqgZK9e92Ea2rcwNfsCke7iUL')
-const SEED_WORD = Buffer.from('Synthetify' + TOKEN_ADDRESS.toString())
-console.log(SEED_WORD.toString())
+const SEED_WORD = Buffer.from(TOKEN_ADDRESS.toString())
 export const getSeedWord = async (seed: Buffer) => {
-  let seedWord = SEED_WORD.slice(0, 31)
+  let seedWord = seed.slice(0, 31)
   const [vestingAccountKey, bump] = await PublicKey.findProgramAddress(
     [seedWord],
     TOKEN_VESTING_PROGRAM_ID
@@ -25,10 +24,10 @@ export const getSeedWord = async (seed: Buffer) => {
   seedWord = Buffer.from(seedWord.toString('hex') + bump.toString(16), 'hex')
   return seedWord
 }
-const main = async () => {
-  const seedWord = await getSeedWord(SEED_WORD)
-  console.log(seedWord.toString('hex'))
-  const info = await getContractInfo(connection, new PublicKey(seedWord))
-  console.log(info)
-}
-main()
+// const main = async () => {
+//   const seedWord = await getSeedWord(SEED_WORD)
+//   console.log(seedWord.toString('hex'))
+//   const info = await getContractInfo(connection, new PublicKey(seedWord))
+//   console.log(info)
+// }
+// main()
--- src/run.ts
+++ src/run.ts
@@ -19,24 +19,29 @@ const connection = new Connection('https://api.mainnet-beta.solana.com')
 const TOKEN_ADDRESS = new PublicKey('xuNGfwG8r3CEubQcssj3A7zoZwb4NaiamhHsJEMt7kf')
 const SOURCE_TOKENS_ADDRESS = new PublicKey('35QuD9QWJDSLKJEmYrfuqgZK9e92Ea2rcwNfsCke7iUL')
 
-const VESTING_START = new Numberu64(74833744)
-const QUARTERLY_SLOTS = new Numberu64(90 * 24 * 60 * 60 * 2) // 15_552_000 assuming 0.5s block time
+const VESTING_START = new Numberu64(1619610925)
+const QUARTERLY_SLOTS = new Numberu64(90 * 24 * 60 * 60) // onde quarter
 
 // fields to change for each investor
-const DESTINATION_ADDRESS = new PublicKey('35QuD9QWJDSLKJEmYrfuqgZK9e92Ea2rcwNfsCke7iUL')
-const AMOUNT = new Numberu64(500 * 1e6) // 6 decimals
-
+const DESTINATION_ADDRESS = new PublicKey('2FjKZ8xoLCkd6R3RGSi4aiXjMDbFBCdJrDRW4qgLbjDh')
+const AMOUNT = new Numberu64(20000000 * 1e6) // 6 decimals
 const SEED = Buffer.from(DESTINATION_ADDRESS.toString())
-
+console.log(SEED.toString('hex'))
 const getSchedule = () => {
   const shedules: Schedule[] = []
-  const initial = AMOUNT.divn(10) // 10% on start
+  const initial = new Numberu64(AMOUNT.divn(10).toString()) // 10% on start
   shedules.push(new Schedule(VESTING_START, initial))
 
-  const quaterlyPart = AMOUNT.sub(initial).divn(16) // 4 years x 4 Quarters Ignore rounding errors
+  const quaterlyPart = new Numberu64(AMOUNT.sub(initial).divn(16).toString()) // 4 years x 4 Quarters Ignore rounding errors
   for (let index = 0; index < 16; index++) {
-    shedules.push(new Schedule(VESTING_START.add(QUARTERLY_SLOTS.muln(index + 1)), quaterlyPart))
+    shedules.push(
+      new Schedule(
+        new Numberu64(VESTING_START.add(QUARTERLY_SLOTS.muln(index + 1)).toString()),
+        quaterlyPart
+      )
+    )
   }
+  console.log(shedules)
   return shedules
 }
 
@@ -47,18 +52,20 @@ const main = async () => {
     },
     derivationPath: DERIVATION_PATH.bip44Root
   }
-
   const wallet = WalletProviderFactory.getProvider(args)
   await wallet.init()
   if (!wallet.publicKey) {
     throw new Error('failed to connect')
   }
+  const seedWord = await getSeedWord(SEED)
+  console.log('seed word')
+  console.log(seedWord.toString('hex'))
 
   const shedule = getSchedule()
   const ixs = await create(
     connection,
     TOKEN_VESTING_PROGRAM_ID,
-    await getSeedWord(SEED),
+    seedWord,
     wallet.publicKey,
     wallet.publicKey,
     SOURCE_TOKENS_ADDRESS,
